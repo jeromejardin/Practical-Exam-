ea6d429afd8de3b1a8a499759f61f1352a5f0be9
jeromejardin <100860566+jeromejardin@users.noreply.github.com>
2022-03-04 00:50:38 +0800

    Update index.php

--- Jardin_Jerome_PracticalExam2/Exam_04/index.php
+++ Jardin_Jerome_PracticalExam2/Exam_04/index.php
@@ -2,9 +2,11 @@
 
 $a = date_create('2019-05-31'); //variable for date
 $b = date_create('2018-04-05');
-
+$date1 = $a->format('Y-m-d') ;
+$date2 = $b->format('Y-m-d') ;
 
 $check = date_diff($a, $b); //to compare the two dates
 
+echo "The Difference of ". $date1. " and ". $date2. " is: <br>";
 echo $check -> format( '%y years %m months %d days' ); //display the difference of the 2 dates by year - month - days
 ?>
