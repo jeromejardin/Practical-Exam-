05406f5ab5cca34cebe04d75a1b50a417b6dcd67
jeromejardin <100860566+jeromejardin@users.noreply.github.com>
2022-03-04 00:50:04 +0800

    Update index.php

--- Jardin_Jerome_PracticalExam2/Exam_05/index.php
+++ Jardin_Jerome_PracticalExam2/Exam_05/index.php
@@ -8,8 +8,10 @@ function rev($word){
 $word = "Wood";
 $laugh = "Laugh";
 echo "The word is: ". $laugh . " and " . $word ; //display the original spelling of the word
-echo "<br> The reversed spelling is: ". rev($laugh) . " and " .$word; //display the reversed spelling of the word
+echo "<br> The reversed spelling is: ". rev($laugh) . " and " .rev($word); //display the reversed spelling of the word
 
-
-
+echo "<br><br> Doing it with numbers";
+$no1 = '1239648';
+echo "The number is: ". $no1  ; //display the original spelling of the word or number format
+echo "<br> The reversed structure is: ". rev($no1) ; //display the reversed spelling of the word or number
 ?>
