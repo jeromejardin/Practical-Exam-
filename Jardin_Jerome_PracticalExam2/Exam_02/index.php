e2e5f1bbf0e5ec6b9ca0842b6ec6555b711b5788
jeromejardin <100860566+jeromejardin@users.noreply.github.com>
2022-03-04 00:23:26 +0800

    Update index.php

--- Jardin_Jerome_PracticalExam2/Exam_02/index.php
+++ Jardin_Jerome_PracticalExam2/Exam_02/index.php
@@ -2,16 +2,16 @@
 
 $newfile = fopen("hello_there.php", "w"); //code to create a new file and write on it
 $content = 
-"<?php
+'<?php
+
 //This is a comment
 
-echo 'This is a test Page!';
-echo 'Exam number two!';
-
-echo'Galileo OJT Practical Exam!';
+echo "This is a test Page!";
+echo "Exam number two!";
 
 //This is the end of a comment
-?>";  //content of the newly created file
+
+?>';  //content of the newly created file
 
 fwrite($newfile, $content); //function to write on the file
 fclose($newfile); //to close the file
