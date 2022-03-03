4f164e51e700765addd42ed38b5466bda7c7fdd9
jeromejardin <100860566+jeromejardin@users.noreply.github.com>
2022-03-04 00:51:46 +0800

    Update index.php

--- Jardin_Jerome_PracticalExam2/Exam_01/index.php
+++ Jardin_Jerome_PracticalExam2/Exam_01/index.php
@@ -8,13 +8,33 @@
         }   
     }
 
-    $word = "madam";
+    $word = "m a d a m";
+    $word2 = "awit";
+    $number = "1345";
+    $number2 = "29892";
     
     //display if the word is palindrome or not
     if(Palin($word)){
-        echo "<br>". $word. " is a Palindrome";
+        echo "<br><br>". $word. " is a Palindrome";
     }else{
-        echo "<br>". $word. " is not Palindrome";
+        echo "<br><br>". $word. " is not Palindrome";
+    }
+
+    if(Palin($word2)){
+        echo "<br><br>". $word2. " is a Palindrome";
+    }else{
+        echo "<br><br>". $word2. " is not Palindrome";
+    }
+
+    if(Palin($number)){
+        echo "<br><br>". $number. " is a Palindrome";
+    }else{
+        echo "<br><br>". $number. " is not Palindrome";
     }
     
+    if(Palin($number2)){
+        echo "<br><br>". $number2. " is a Palindrome";
+    }else{
+        echo "<br><br>". $number2. " is not Palindrome";
+    }
 ?>
